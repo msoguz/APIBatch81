0e412251c5501cb547afeff727ce880b8683896f
M. Sener OGUZ <mseneroguz@gmail.com>
2022-11-03 22:53:02 +0300

    .

---
+++ src/test/java/post_requests/Post01.java
@@ -0,0 +1,73 @@
+package post_requests;
+
+import base_url.JsonplaceholderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+import test_data.JsonPlaceHolderTestData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Post01 extends JsonplaceholderBaseUrl {
+
+    /*
+    Given
+      1) https://jsonplaceholder.typicode.com/todos
+      2) {
+            "userId": 55,
+            "title": "Tidy your room",
+            "completed": false
+                         }
+   When
+       I send POST Request to the Url
+   Then
+       Status code is 201
+   And
+       response body is like {
+                               "userId": 55,
+                               "title": "Tidy your room",
+                               "completed": false,
+                               "id": 201
+                               }
+*/
+
+    @Test
+    public void post01(){
+
+        //1-Set the url
+        spec.pathParam("first","todos");
+
+        //2- Set the expected data
+        JsonPlaceHolderTestData obj=new JsonPlaceHolderTestData();
+        Map<String, Object > expectedData= obj.expectedDataMethod(55, "Tidy your room", false); //Bizim payload'imiz.(Expected datamiz)
+
+        //3- Send The Reques and Get The Response
+        Response response=given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().post("/{first}");
+        response.prettyPrint();
+
+
+        //Do Assertion
+        Map<String,Object>  actualData=response.as(HashMap.class);
+
+        assertEquals(expectedData.get("completed"),actualData.get("completed"));
+        assertEquals(expectedData.get("tıtle"),actualData.get("tıtle"));
+        assertEquals(expectedData.get("userId"),actualData.get("userId"));
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
